20e65cf79dfe66f133ebe2e3728834f2a33de648
David Morales <dmoralesx24@gmail.com>
2019-08-24 12:13:04 -0400

    got the burger.js file running

--- model/burger.js
+++ model/burger.js
@@ -6,6 +6,25 @@ const orm = require("../config/orm.js");
 
 let burger = {
     all: function(cb) {
-        orm.all("burgers")
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+            cb(res);
+        });
     }
-}
+};
+
+module.exports = burger;
