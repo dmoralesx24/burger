1c33321af38accfe03bf9c4dd60cea48ada0a568
David Morales <dmoralesx24@gmail.com>
2019-08-24 11:33:10 -0400

    added the orm file and finished my controller file

--- controller/burgers_controller.js
+++ controller/burgers_controller.js
@@ -30,5 +30,29 @@ router.post("/api/burgers", function(req, res) {
 router.put("/api/burgers/:id", function(req, res) {
     let condition = "id = " + req.params.id;
 
-    console.log()
-})
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
--- model/burger.js
+++ model/burger.js
@@ -2,3 +2,10 @@
 // this file is going to call the orm functions
 // and using specific input for it 
 // export it at the end
+const orm = require("../config/orm.js");
+
+let burger = {
+    all: function(cb) {
+        orm.all("burgers")
+    }
+}
